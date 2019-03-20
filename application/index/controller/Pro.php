9bd09dd941ea9a0a81c98f564e27d5ce3e71bf31
zhuliren <1029422171@qq.com>
2019-03-20 17:39:58 +0800

    增加用户登录接口、增加项目详情接口（未完善）

--- application/index/controller/Pro.php
+++ application/index/controller/Pro.php
@@ -15,15 +15,58 @@ class Pro
 {
     public function proList()
     {
-        $prolist = Db::query('SELECT a.`id`,a.`pro_name`,a.`pro_originator`,a.`pro_headimg`,b.`value` AS pro_state FROM xm_tbl_pro a LEFT JOIN xm_tbl_dictionary b ON a.`pro_state`=b.`id`');
-        $data = array('status' => 0, 'msg' => '成功', 'data' => $prolist);
+        //项目列表
+        $prolist = Db::query('SELECT a.`id`,a.`pro_name`,a.`pro_originator`,a.`pro_headimg`,b.`value` AS pro_state ,COUNT(c.id) AS pro_innum FROM xm_tbl_pro a LEFT JOIN xm_tbl_dictionary b ON a.`pro_state`=b.`id` LEFT JOIN xm_tbl_pro_card c ON a.`id`=c.`pro_id`');
+        $prolistnum = count($prolist);
+        $datadetails = array('listnum' => $prolistnum, 'listdata' => $prolist);
+        $data = array('status' => 0, 'msg' => '成功', 'data' => $datadetails);
         return json($data);
     }
 
     public function proDetails()
     {
+//        $user_id = $_REQUEST['userid'];
+//        $pro_id = $_REQUEST['proid'];
+        $user_id = 1;
+        $pro_id = 1;
+        //昨日时间和今日时间点
+        //项目基础数据
+        $stardatetime = date("Y-m-d", strtotime("-1 day"));
+        $enddatetime = date("Y-m-d");
+        $pro_data = Db::table('xm_tbl_pro_data')
+            ->where('pro_id', $pro_id)
+            ->whereTime('pro_datatime', 'between', [$stardatetime, $enddatetime])
+            ->select();
+        //昨日数据绑定
+        $pro_datadetails = array();
+        $n = 0;
+        foreach ($pro_data as $arr) {
+            $pro_datadetails[$n]['pro_dataname'] = Db::table('xm_tbl_dictionary')->where('id', $arr['pro_dataname'])->value('value');
+            $pro_datadetails[$n]['pro_datavalue'] = $arr['pro_datavalue'];
+            $pro_datadetails[$n]['pro_daygrow'] = 0;
+            $pro_datadetails[$n]['pro_weekgrow'] = 0;
+            $pro_datadetails[$n]['pro_mongrow'] = 0;
+            $n++;
+        }
+        $pro_yesterday_datadetails = array('pro_datadetails_num' => $n, 'pro_datadetails' => $pro_datadetails);
+        //项目信息
+        $pro_finance = Db::table('xm_tbl_pro_finance')->where('pro_id', $pro_id)->find();
+        #编写位置注释
+        $prolist = Db::query('SELECT a.`pro_name`,a.`pro_originator`,b.`value` AS pro_state FROM xm_tbl_pro a LEFT JOIN xm_tbl_dictionary b ON a.`pro_state`= b.`id` where a.id= ? ',[$pro_id]);
+        return json($pro_finance);
+        //重组数据
+        $userdetails = db('xm_tbl_user')->where('id', $user_id)->find();
+        //判断用户权限
+        if ($userdetails['up_code'] == null) {
+            //普通用户
+
+        } else {
+            //被邀请用户
+        }
         $pro_id = $_REQUEST["pro_id"];
         $prodetails = Db::query('select * from xm_tbl_pro where id = ?', [$pro_id]);
+        $prodetails = array();
+        array('status' => 0, 'msg' => '成功', 'data' => $prodetails);
         return json($prodetails);
     }
 }
--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -9,12 +9,13 @@
 namespace app\index\controller;
 
 
+use think\Db;
+
 class user
 {
     public function userRegister()
     {
-//        $code  =  $_REQUEST['code'];
-        $code = "te6565656st";
+        $code = $_REQUEST['code'];
         $appid = "wx4473d33d20a8d3b3";
         $secret = "a1904ad7e0ab761657a294bc00352c3d";
         $URL = "https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$secret&js_code=$code&grant_type=authorization_code";
@@ -22,17 +23,57 @@ class user
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $URL);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
         curl_setopt($ch, CURLOPT_REFERER, '');
         $output = curl_exec($ch);
         curl_close($ch);
-        return json($output);
+        $output = json_decode($output, true);
+        if (isset($output['openid']) || (isset($output['errcode']) ? $output['errcode'] : 0) == 0) {
+            $openid = $output['openid'];
+            //查询是否有该openid在数据库中
+            $userdetails = Db::query('SELECT * FROM xm_tbl_user WHERE wechat_open_id = ?', [$openid]);
+            if (count($userdetails) == 0) {
+                //无用户信息，插入用户信息
+                $userdata = ['wechat_open_id' => $openid, 'created_time' => date("Y-m-d H:i:s" ,time())];
+                $user_id = Db::table('xm_tbl_user')->insertGetId($userdata);
+                $returndata = array('user_id' => $user_id, 'openid' => $openid, 'user_type' => '0', 'user_type_msg' => '普通用户');
+                $data = array('status' => 0, 'msg' => '登录成功', 'data' => $returndata);
+                return json($data);
+            } else {
+                //返回用户信息
+                $userdetails = db('xm_tbl_user')->where('wechat_open_id', $openid)->find();
+                if ($userdetails['up_code'] == null) {
+                    $user_type_msg = '普通用户';
+                    $user_type = '0';
+                } else {
+                    $user_type_msg = '被邀请用户';
+                    $user_type = '1';
+                }
+                $returndata = array('user_id' => $userdetails['id'], 'openid' => $openid, 'user_type' => $user_type, 'user_type_msg' => $user_type_msg);
+                $data = array('status' => 0, 'msg' => '登录成功', 'data' => $returndata);
+                return json($data);
+            }
+        } else if ($output['errcode'] == 40029) {
+            $data = array('status' => 1, 'msg' => 'code无效', 'data' => '');
+            return json($data);
+        } else if ($output['errcode'] == 45011) {
+            $data = array('status' => 1, 'msg' => '频率限制，每个用户每分钟100次', 'data' => '');
+            return json($data);
+        } else if ($output['errcode'] == -1) {
+            $data = array('status' => 1, 'msg' => '微信系统繁忙稍后再试', 'data' => '');
+            return json($data);
+        }else if ($output['errcode'] == 40163) {
+            $data = array('status' => 1, 'msg' => 'code已经被使用了', 'data' => '');
+            return json($data);
+        }
     }
 
-    public function userLogin()
+    public
+    function userInfoSet()
     {
-        $user_id = $_REQUEST['user_id'];
-        return $user_id;
+
+        return date("Y-m-d H:i:s" ,time());
     }
 }
