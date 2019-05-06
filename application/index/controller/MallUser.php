85652b987da5b1dd781a6957a00fd38204738c61
duheyuan <duheyuan@aliyun.com>
2019-05-06 19:37:02 +0800

    session_key修改

--- application/index/controller/MallUser.php
+++ application/index/controller/MallUser.php
@@ -421,11 +421,8 @@ class MallUser
         }else{
             return json(['status'=>0,'msg'=>'2失败','data'=>'']);
         }
-        if (isset($all['session_key']) && !empty($all['session_key'])){
-            $session_key = $all['session_key'];
-        }else{
-            return json(['status'=>0,'msg'=>'缺少session_key参数','data'=>'']);
-        }
+        $session_key = $this->getAccessToken($this->appid,$this->secret);
+
         if (isset($all['iv']) && !empty($all['iv'])){
             $iv = $all['iv'];
         }else{
@@ -596,6 +593,39 @@ class MallUser
             return false;
         } else {
             return $tmpInfo;
+        }
+    }
+
+
+    public function getAccessToken($appid,$secret){
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
+        $res = $this->curl_get($url);
+        $res = json_decode($res,1);
+        if (isset($res['errcode'])){
+            if($res['errcode']!=0) throw new Exception($res['errmsg']);
+        }
+        return $res['access_token'];
+    }
+    public function curl_get($url)
+    {
+        $headers = array('User-Agent:Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36');
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+        }
+        curl_setopt($oCurl, CURLOPT_TIMEOUT, 20);
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
         }
     }
 
